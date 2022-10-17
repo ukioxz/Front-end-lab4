dfd30665e954e752fc2be5035ce7849598b2b367
ukioxz <ikatember@gmail.com>
2022-10-18 00:08:47 +0300

    Add add button

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const allChar ="0123456789ABCDEF";
 let firstElem = document.getElementById('full_name');
 let secondElem = document.querySelector('.my_date');
 
+let addBtn = document.querySelector('.bt1');
+let img1 = document.getElementById('block');
+let fileI = document.getElementById('file1');
+
+function save(){
+  let f = fileI.files[0];
+  if (f) {
+      img1.src = URL.createObjectURL(f);
+      localStorage.setItem('myImage', img1.src);
+  }
+}
+img1.src = localStorage.getItem('myImage');
+
 const randomSetColorFont = () => {
   let randomColor = '';
   for(let i = 0; i < 6; i++){
